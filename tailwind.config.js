3053d7dc9b1ac119e2323a93fa62777b869d94bf
fredhel KISSIE <fredkiss3@gmail.com>
2022-07-09 05:54:22 +0200

    :lipstick: Updated tailwind configuration to the design-system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,35 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'lc-primary': {
-          100: `#f76b0b`,
-          80: `#CA5402`,
+        primary: {
+          DEFAULT: `#f76b0b`,
+          75: `#CA5402`,
+          15: `#CA5402`,
         },
-        'lc-dark': `#3a3a3a`,
-        'lc-darker': `#3a3335`,
-        'lc-gray': `#ECEDEE`,
+        secondary: {
+          DEFAULT: `#2AC084`,
+          75: `#209063`,
+          15: `#dff6ed`,
+        },
+        dark: {
+          DEFAULT: `#3a3335`,
+        },
+        gray: {
+          DEFAULT: `#908E8E`,
+        },
+        lightgray: {
+          DEFAULT: `#EFEFEF`
+        },
+        accent: {
+          primary: {
+            DEFAULT: `#FDF0D5`,
+            70: "#eef3f2",
+          },
+          secondary: {
+            DEFAULT: `#C6D8D3`,
+            60: "#eef3f2",
+          },
+        },
       },
     },
   },
