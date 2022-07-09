99c6ea802f25dbc7de4bdba30e7f6a6ff185c730
fredhel KISSIE <fredkiss3@gmail.com>
2022-07-09 05:56:43 +0200

    :lipstick: Button component finished

--- ui/src/components/button.tsx
+++ ui/src/components/button.tsx
@@ -1,17 +1,87 @@
 import * as React from "react";
+import { clsx } from "../lib/functions";
 
-export type ButtonProps = {
+export interface ButtonProps {
+  title?: string;
+  renderLeadingIcon?: (classNames: string) => JSX.Element;
+  renderTrailingIcon?: (classNames: string) => JSX.Element;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  disabled?: boolean;
   children?: React.ReactNode;
-  onClick?: () => void;
-};
+  variant?: "primary" | "secondary" | "hollow";
+  type?: "button" | "submit" | "reset";
+  ariaLabel?: string;
+  square?: boolean;
+  className?: string;
+  loading?: boolean;
+}
 
-export function Button({ children, onClick }: ButtonProps) {
+export function Button({
+  children,
+  onClick,
+  ariaLabel,
+  renderTrailingIcon,
+  renderLeadingIcon,
+  className,
+  title,
+  type,
+  loading,
+  disabled,
+  square,
+  variant = `hollow`,
+}: ButtonProps) {
   return (
     <button
-      className={`bg-lc-primary-100 text-white px-4 py-2 rounded-md`}
+      title={title}
+      disabled={disabled || loading}
+      aria-label={ariaLabel}
+      type={type}
+      className={clsx(
+        className,
+        `px-4 py-2 rounded-md flex gap-2 items-center`,
+        `font-bold focus:ring`,
+        {
+          "!px-2": !!square,
+          "bg-primary hover:bg-primary-75 active:bg-primary-75 ring-primary/50 text-white":
+            variant === `primary`,
+          "bg-secondary hover:bg-secondary-75 active:bg-secondary-75 ring-secondary/50 text-white":
+            variant === "secondary",
+          "bg-lightgray text-dark ring-gray/50": variant === "hollow",
+          "bg-gray !text-white cursor-not-allowed hover:bg-gray active:bg-gray":
+            !!loading || !!disabled,
+        }
+      )}
       onClick={onClick}
     >
+      {loading && <LoadingIndicator className={`h-5 w-5`} />}
+      {!loading && renderLeadingIcon && renderLeadingIcon(`h-5 w-5`)}
       {children}
+      {renderTrailingIcon && renderTrailingIcon(`h-5 w-5`)}
     </button>
+  );
+}
+
+function LoadingIndicator({ className }: { className?: string }) {
+  return (
+    <svg
+      className={clsx(`animate-spin`, className)}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
   );
 }
--- ui/src/stories/button.stories.tsx
+++ ui/src/stories/button.stories.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Button, ButtonProps } from "../components/button";
+import { DotsThree, Smiley } from "phosphor-react";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Design-System/Button",
   component: Button,
+  argTypes: {
+    variant: {
+      control: `select`,
+    },
+    type: {
+      control: `select`,
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -16,4 +25,34 @@ export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
   children: "Button",
+} as ButtonProps;
+
+export const WithLeadingIcon = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithLeadingIcon.args = {
+  children: "Button",
+  renderLeadingIcon: (cls) => <Smiley className={cls} />,
+} as ButtonProps;
+
+export const WithTrailingIcon = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithTrailingIcon.args = {
+  children: "Button",
+  renderTrailingIcon: (cls) => <Smiley className={`${cls} rotate-180`} />,
+} as ButtonProps;
+
+export const WithBothIcons = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+WithBothIcons.args = {
+  children: "Button",
+  renderLeadingIcon: (cls) => <Smiley className={cls} />,
+  renderTrailingIcon: (cls) => <Smiley className={`${cls} rotate-180`} />,
+} as ButtonProps;
+
+export const Square = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Square.args = {
+  renderLeadingIcon: (cls) => <DotsThree className={cls}  weight="bold" />,
+  variant: `primary`,
+  square: true,
 } as ButtonProps;
