0a876aeabb0a42c8f2acf866f517aeee685aff8c
fredhel KISSIE <fredkiss3@gmail.com>
2022-07-25 02:11:43 +0200

    ♻️ Do not test requests because it not needed

--- domain/scripts/templates/usecases/tests/usecases/__UC__UseCase.spec.ts
+++ domain/scripts/templates/usecases/tests/usecases/__UC__UseCase.spec.ts
@@ -33,32 +33,7 @@ describe('__UC__ Use case', () => {
         expect(presenter.response).not.toBe(null);
     });
 
-    it.todo('Negative Use Case');
-
-    describe('Invalid Requests', () => {
-        const dataset: { label: string; request: __UC__Request }[] = [
-            // TODO: Specify requests
-            {
-                label: 'Example label',
-                request: {
-                    ...request
-                }
-            }
-        ];
-
-        it.each(dataset)(
-            'shows errors with invalid request : "$label"',
-            async ({ request }) => {
-                // // Given
-                // const useCase = new __UC__UseCase();
-                //
-                // // When
-                // await useCase.execute(request, presenter);
-                //
-                // // Then
-                // expect(presenter.response.errors).not.toBe(null);
-                expect(true).toBe(false);
-            }
-        );
+    it('Is a Negative Use Case', async () => {
+        expect(false).toBe(true);
     });
 });
