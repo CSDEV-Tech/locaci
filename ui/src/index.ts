32a648aa40c2dd932d78aa51f508203b12cf1219
fredhel KISSIE <fredkiss3@gmail.com>
2022-08-17 03:49:44 +0200

    💄 Export the newly created components in UI

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,6 +1,13 @@
+// atoms
 export * from './components/atoms/button';
 export * from './components/atoms/tag';
 export * from './components/atoms/card';
 export * from './components/atoms/switch';
 export * from './components/atoms/input';
+export * from './components/atoms/checkbox';
+export * from './components/atoms/radio';
+// molecules
+export * from './components/molecules/pagination';
+export * from './components/molecules/checkbox-group';
+export * from './components/molecules/radio-group';
 export * from './lib/functions';
