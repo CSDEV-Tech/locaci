fd66e3e429cf75fa53bed97819f6fdb92b5d035a
fredhel KISSIE <fredkiss3@gmail.com>
2022-08-27 00:12:43 +0200

    ♿️ Added accessibility add-on for storybook

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -1,31 +1,32 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
+    stories: [
+        '../src/**/*.stories.mdx',
+        '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss')
+                }
+            }
         },
-      },
+        '@storybook/addon-a11y'
+    ],
+    framework: '@storybook/react',
+    core: {
+        disableTelemetry: true // 👈 Disables telemetry
     },
-  ],
-  "framework": "@storybook/react",
-  core: {
-    disableTelemetry: true, // 👈 Disables telemetry
-  },
-  webpackFinal: async config => {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    });
-    return config;
-  },
-}
+    webpackFinal: async config => {
+        config.module.rules.push({
+            test: /\.mjs$/,
+            include: /node_modules/,
+            type: 'javascript/auto'
+        });
+        return config;
+    }
+};
