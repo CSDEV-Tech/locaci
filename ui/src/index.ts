ecba6c93376363c4b777ccee54eff0c63f30085d
fredhel KISSIE <fredkiss3@gmail.com>
2022-08-30 16:29:31 +0200

    💄  Added the export the newly created components in ui package

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,3 +1,5 @@
+export * from './lib/functions';
+
 // atoms
 export * from './components/atoms/button';
 export * from './components/atoms/tag';
@@ -9,8 +11,17 @@ export * from './components/atoms/radio';
 export * from './components/atoms/combobox';
 export * from './components/atoms/select';
 export * from './components/atoms/progress';
+export * from './components/atoms/loading-indicator';
+export * from './components/atoms/modal';
+export * from './components/atoms/search-button';
+export * from './components/atoms/link-button';
+
 // molecules
 export * from './components/molecules/pagination';
 export * from './components/molecules/checkbox-group';
 export * from './components/molecules/radio-group';
-export * from './lib/functions';
+export * from './components/molecules/footer';
+export * from './components/molecules/map-pin';
+
+// organisms
+export * from './components/organisms/header';
