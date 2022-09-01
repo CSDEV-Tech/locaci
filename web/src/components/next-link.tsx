fde67a7979fc203e809dd766c79bff688a8135ae
fredhel KISSIE <fredkiss3@gmail.com>
2022-09-02 01:34:57 +0200

    🐛  Use next link and linkbutton to not repeat the instanciation of link component

---
+++ web/src/components/next-link.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { CustomLink, LinkButton, LinkButtonProps } from '@locaci/ui';
+
+export function NextLink(props: CustomLink) {
+    return <Link {...props} />;
+}
+
+export function NextLinkButton(props: Omit<LinkButtonProps, 'Custom'>) {
+    return <LinkButton {...props} Custom={NextLink} />;
+}
