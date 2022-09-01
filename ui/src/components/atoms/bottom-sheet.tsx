c7b1a912a266f5e8188b5a921c55674badb525fd
fredhel KISSIE <fredkiss3@gmail.com>
2022-09-01 04:50:17 +0200

    💄  Added bottom-sheet component

---
+++ ui/src/components/atoms/bottom-sheet.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import {
+    BottomSheet as ReactSpringBottomSheet,
+    BottomSheetProps as ReactSpringBottomSheetProps,
+    BottomSheetRef
+} from 'react-spring-bottom-sheet';
+import 'react-spring-bottom-sheet/dist/style.css';
+
+import { clsx } from '../../lib/functions';
+
+export type BottomSheetProps = {
+    className?: string;
+    children?:
+        | React.ReactNode
+        | ((
+              sheetRef: React.RefObject<BottomSheetRef>,
+              dismiss?: () => void
+          ) => React.ReactNode);
+} & ReactSpringBottomSheetProps;
+
+export function BottomSheet({
+    className,
+    children,
+    open = true,
+    ...props
+}: BottomSheetProps) {
+    const sheetRef = React.useRef<BottomSheetRef>(null);
+    return (
+        <div>
+            <ReactSpringBottomSheet
+                open={open}
+                ref={sheetRef}
+                className={clsx(className)}
+                {...props}>
+                {typeof children === 'function'
+                    ? children(sheetRef, props.onDismiss)
+                    : children}
+            </ReactSpringBottomSheet>
+        </div>
+    );
+}
---
+++ ui/src/stories/atoms/bottom-sheet.stories.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import {
+    BottomSheet,
+    BottomSheetProps
+} from '../../components/atoms/bottom-sheet';
+import { Button } from '../../components/atoms/button';
+import { X } from 'phosphor-react';
+
+export default {
+    title: 'Composants/Atoms/BottomSheet',
+    component: BottomSheet
+} as ComponentMeta<typeof BottomSheet>;
+
+// 👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof BottomSheet> = args => (
+    <>
+        <BottomSheet {...args} />
+    </>
+);
+
+export const Default: ComponentStory<typeof BottomSheet> = args => {
+    const [isOpen, setIsOpen] = React.useState(args.open ?? true);
+    return (
+        <>
+            <Button variant="primary" onClick={() => setIsOpen(true)}>
+                Open BottomSheet
+            </Button>
+            <BottomSheet
+                {...args}
+                open={isOpen}
+                onDismiss={() => setIsOpen(false)}
+            />
+        </>
+    );
+};
+
+Default.args = {
+    onDismiss: undefined,
+    blocking: false,
+    defaultSnap: ({ maxHeight }) => maxHeight / 2,
+    snapPoints: ({ maxHeight }) => [maxHeight, maxHeight / 2, maxHeight / 4],
+    children: (sheetRef, dismiss) => (
+        <>
+            <div className="flex w-full justify-between gap-2 p-4">
+                <Button
+                    variant="primary"
+                    onClick={() => {
+                        sheetRef.current?.snapTo(({ maxHeight }) => maxHeight);
+                    }}>
+                    Snap to max
+                </Button>
+                <Button
+                    variant="secondary"
+                    onClick={() => {
+                        sheetRef.current?.snapTo(
+                            ({ maxHeight }) => maxHeight / 2
+                        );
+                    }}>
+                    Snap to middle
+                </Button>
+                <Button
+                    variant="dark"
+                    onClick={() => {
+                        sheetRef.current?.snapTo(({ minHeight }) => minHeight);
+                    }}>
+                    Snap to min
+                </Button>
+                <Button
+                    variant="hollow"
+                    onClick={() => {
+                        dismiss?.();
+                    }}
+                    renderTrailingIcon={cls => <X className={cls} />}>
+                    Close
+                </Button>
+            </div>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit ex vero reiciendis laborum, iure aut est fugit quibusdam
+                asperiores voluptate illo recusandae officiis ipsam sunt. Dicta
+                vitae eos eius officia?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit ex vero reiciendis laborum, iure aut est fugit quibusdam
+                asperiores voluptate illo recusandae officiis ipsam sunt. Dicta
+                vitae eos eius officia?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit ex vero reiciendis laborum, iure aut est fugit quibusdam
+                asperiores voluptate illo recusandae officiis ipsam sunt. Dicta
+                vitae eos eius officia?
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Impedit ex vero reiciendis laborum, iure aut est fugit quibusdam
+                asperiores voluptate illo recusandae officiis ipsam sunt. Dicta
+                vitae eos eius officia?
+            </p>
+        </>
+    )
+} as BottomSheetProps;
