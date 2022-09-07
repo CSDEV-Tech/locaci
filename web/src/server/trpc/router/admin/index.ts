dc25dea812da87f9c5961ab91c6c41d2337d3091
fredhel KISSIE <fredkiss3@gmail.com>
2022-09-08 00:03:36 +0200

    ✨  Admin Dashboard : Accept & Refuse access

---
+++ web/src/server/trpc/router/admin/index.ts
@@ -0,0 +1,73 @@
+import { Uuid } from '@locaci/domain';
+import { z } from 'zod';
+import { t } from '../../trpc-server-root';
+import { isAdmin } from '../../middleware/auth';
+import { TRPCError } from '@trpc/server';
+import jwt from 'jsonwebtoken';
+import { env } from 'web/src/env/server.mjs';
+import { RequestStatus } from '@prisma/client';
+import { refuseOwnerAccessSchema } from '../../validation/auth-schema';
+
+const protectedProcedure = t.procedure.use(isAdmin);
+
+export const adminRouter = t.router({
+    getAllRequests: protectedProcedure.query(async ({ ctx, input }) => {
+        return await ctx.prisma.requestOwnerRole.findMany({
+            orderBy: {
+                createdAt: `desc`
+            },
+            include: {
+                user: true
+            }
+        });
+    }),
+    generateLinkForOwner: protectedProcedure
+        .input(
+            z.object({
+                uid: z.string().uuid(),
+                host: z.string().url()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            // create the request if it does not exists, else do nothing
+            const request = await ctx.prisma.requestOwnerRole.findUnique({
+                where: {
+                    id: input.uid
+                }
+            });
+
+            if (!request) {
+                throw new TRPCError({ code: 'NOT_FOUND' });
+            }
+
+            const token = jwt.sign(
+                {
+                    id: new Uuid(request.id).short()
+                },
+                env.JWT_SECRET,
+                {
+                    expiresIn: `1d`, // only for one day
+                    algorithm: 'HS256'
+                }
+            );
+
+            return `${input.host}/auth/confirm-access/${token}`;
+        }),
+    refuseOwnerAccess: protectedProcedure
+        .input(refuseOwnerAccessSchema)
+        .mutation(async ({ ctx, input: { uid, reason } }) => {
+            // pass state to granted because the user has validated now
+            const request = await ctx.prisma.requestOwnerRole.update({
+                where: {
+                    id: uid.toString()
+                },
+                data: {
+                    status: RequestStatus.REFUSED,
+                    reasonForRefusing: reason
+                }
+            });
+
+            // TODO : Send Email to owner with reason
+            return { success: true };
+        })
+});
--- web/src/server/trpc/router/index.ts
+++ web/src/server/trpc/router/index.ts
@@ -1,10 +1,12 @@
 import { t } from '../trpc-server-root';
+import { adminRouter } from './admin/index';
 import { authRouter } from './auth';
 import { propertyRouter } from './property';
 
 export const appRouter = t.router({
     property: propertyRouter,
-    auth: authRouter
+    auth: authRouter,
+    admin: adminRouter
 });
 
 // export type definition of API
