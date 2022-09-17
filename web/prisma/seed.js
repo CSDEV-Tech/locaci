98119597cc8334986197c27eec76d3fe9df652a8
fredhel KISSIE <fredkiss3@gmail.com>
2022-09-17 20:41:29 +0200

    🗃️ Added cities table & dataset

---
+++ web/prisma/seed.js
@@ -0,0 +1,56 @@
+const { PrismaClient } = require('@prisma/client');
+const yaml = require('js-yaml');
+const { promises: fs } = require('node:fs');
+const path = require('node:path');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    try {
+        const data = yaml.load(
+            await fs.readFile(
+                path.join(path.resolve(__dirname, 'city-data.yaml')),
+                'utf8'
+            )
+        );
+
+        console.log(`Start seeding ...`);
+        console.log(`Empting cities table ...`);
+        await prisma.locality.deleteMany();
+        await prisma.municipality.deleteMany();
+        await prisma.city.deleteMany();
+
+        console.log(`Recreating cities table ...`);
+        for (const city of data.cities) {
+            const cityEntity = await prisma.city.create({
+                data: {
+                    name: city.name.toLowerCase(),
+                    municipalities: {
+                        create: city.municipalities.map(m => {
+                            return {
+                                name: m.name.toLowerCase(),
+                                localities: {
+                                    create: m.localities
+                                }
+                            };
+                        })
+                    }
+                }
+            });
+            console.log(`Created city with id: ${cityEntity.id}`);
+        }
+        console.log(`Seeding finished.`);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async e => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
