601596f5156d906465a96de44ce4e072313f39dc
Adrien KISSIE <fredkiss3@gmail.com>
2022-12-11 18:10:38 +0100

    👷 First shot a CI/CD script

---
+++ copy-env.sh
@@ -0,0 +1,10 @@
+export POSTGRES_USER=$(cat /run/secrets/locaci_postgres_user)
+export POSTGRES_PASSWORD=$(cat /run/secrets/locaci_postgres_password)
+export CF_ACCESS_KEY_ID=$(cat /run/secrets/locaci_cf_access_key_id)
+export CF_ACCESS_KEY_SECRET=$(cat /run/secrets/locaci_cf_access_key_secret)
+export CF_ACCOUNT_ID=$(cat /run/secrets/locaci_cf_account_id)
+export CF_IMAGES_BUCKET_NAME=$(cat /run/secrets/locaci_cf_images_bucket_name)
+export CF_IMAGES_DOMAIN=$(cat /run/secrets/locaci_cf_images_domain)
+export JWT_SECRET=$(cat /run/secrets/locaci_jwt_secret)
+export OAUTH_CLIEND_SECRET=$(cat /run/secrets/locaci_oauth_cliend_secret)
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/locaci_db?schema=public"
