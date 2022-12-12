aece443f4885b23e6c1d896861fed6df504871e7
Adrien KISSIE <fredkiss3@gmail.com>
2022-12-12 10:32:07 +0100

    💚 change startup script

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,4 @@
+# export ENV variables
 export POSTGRES_USER=$(cat /run/secrets/locaci_postgres_user)
 export POSTGRES_PASSWORD=$(cat /run/secrets/locaci_postgres_password)
 export CF_ACCESS_KEY_ID=$(cat /run/secrets/locaci_cf_access_key_id)
@@ -8,3 +9,6 @@ export CF_IMAGES_DOMAIN=$(cat /run/secrets/locaci_cf_images_domain)
 export JWT_SECRET=$(cat /run/secrets/locaci_jwt_secret)
 export OAUTH_CLIEND_SECRET=$(cat /run/secrets/locaci_oauth_cliend_secret)
 export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/locaci_db?schema=public"
+
+# Run pnpm
+pnpm run start:web
