45a33c8e8de564013cdaac0524270c7e80ca6c0d
Adrien KISSIE <fredkiss3@gmail.com>
2022-12-14 20:42:46 +0100

    🔍 replace favicon svg with favicon.ico

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({ children }: LayoutProps) {
     return (
         <html lang="fr" className={poppins.className}>
             <head>
-                <link rel="icon" href={`/favicon.svg`} />
                 <meta name="viewport" content="width=device-width" />
                 <meta name="charset" content="utf-8" />
             </head>
