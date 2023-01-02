b82cbcee6e51c6318edf2947149ab9b9961abb45
Adrien KISSIE <fredkiss3@gmail.com>
2023-01-02 21:27:28 +0100

    🚧  property booking started on backend

--- web/src/validation/booking-schema.ts
+++ web/src/validation/booking-schema.ts
@@ -17,21 +17,22 @@ export const bookPropertySchema = z.object({
         .min(1, 'Veuillez remplir ce champ pour pouvoir faire la réservation'),
     phoneNumber: z.preprocess(
         val => {
-            const newVal = String(val);
-            return newVal.replace(/[ -]/g, '');
+            return String(val).replace(/[ -]/g, '');
         },
         z
             .string({
                 required_error: `Veuillez saisir votre numéro de téléphone`
             })
             .regex(
-                /([+]?\d{3}?|)(\d{10})/g,
+                /^([+]?\d{3}?|)(\d{10})$/,
                 `Veuillez saisir un numéro de téléphone valide, c'est-à-dire un numéro à 10 chiffres.  
                     ex: +2250148351513, ou 0648351513`
             )
     ),
-    date: z
-        .date()
+    bookingDate: z
+        .date({
+            required_error: `Veuillez saisir une date de réservation`
+        })
         .refine(
             date =>
                 convertDateToBeginOfDate(date) >=
