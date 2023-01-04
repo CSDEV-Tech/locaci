6cdba2657ea3662230e63842c97386a1d18263bd
Adrien KISSIE <fredkiss3@gmail.com>
2023-01-04 11:00:38 +0100

    🐛 remove prefetch on dynamic-link to always get fresh data on the click to the link

--- web/src/features/shared/components/next-link.tsx
+++ web/src/features/shared/components/next-link.tsx
@@ -1,101 +1,53 @@
-'use client';
-import * as React from 'react';
+"use client";
+import * as React from "react";
 // components
-import Link from 'next/link';
+import Link from "next/link";
 import {
-    LinkButton,
-    type LinkButtonProps
-} from '@locaci/ui/components/atoms/link-button';
+  LinkButton,
+  type LinkButtonProps,
+} from "@locaci/ui/components/atoms/link-button";
 
 // utils
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 // types
-import type { CustomLink } from '@locaci/ui/components/atoms/link';
+import type { CustomLink } from "@locaci/ui/components/atoms/link";
 
 export const NextLink = React.forwardRef<HTMLAnchorElement, CustomLink>(
-    (props, ref) => {
-        return <Link {...props} ref={ref} />;
-    }
+  (props, ref) => {
+    return <Link {...props} ref={ref} />;
+  }
 );
 
-export function NextLinkButton(props: Omit<LinkButtonProps, 'Custom'>) {
-    return <LinkButton {...props} Custom={NextLink} />;
+export function NextLinkButton(props: Omit<LinkButtonProps, "Custom">) {
+  return <LinkButton {...props} Custom={NextLink} />;
 }
 
-export function NextDynamicLinkButton(props: Omit<LinkButtonProps, 'Custom'>) {
-    return <LinkButton {...props} Custom={NextDynamicLink} />;
+export function NextDynamicLinkButton(props: Omit<LinkButtonProps, "Custom">) {
+  return <LinkButton {...props} Custom={NextDynamicLink} />;
 }
 
 export const NextDynamicLink = React.forwardRef<HTMLAnchorElement, CustomLink>(
-    ({ href, children, ...props }, forwardedRef) => {
-        const router = useRouter();
-        const [hasBeenPrefetched, setHasBeenPrefetched] = React.useState(false);
-        const [isPrefetching, startTransition] = React.useTransition();
-        const localRef = React.useRef<HTMLAnchorElement>();
-
-        React.useEffect(() => {
-            if (localRef.current) {
-                const options = {
-                    rootMargin: '0px',
-                    threshold: 0.5
-                };
-
-                const observer = new IntersectionObserver(entries => {
-                    entries.forEach(entry => {
-                        if (entry.isIntersecting) {
-                            if (!hasBeenPrefetched && !isPrefetching) {
-                                // eagerly prefetch route for faster route transition, when link is visible
-                                startTransition(() => {
-                                    router.prefetch(href);
-                                    setHasBeenPrefetched(true);
-
-                                    // remove IntersectionObserver on finish
-                                    if (localRef.current) {
-                                        observer.unobserve(localRef.current);
-                                    }
-                                });
-                            }
-                        }
-                    });
-                }, options);
-
-                observer.observe(localRef.current);
-
-                return () => {
-                    // remove IntersectionObserver on unMount
-                    if (localRef.current) {
-                        observer.unobserve(localRef.current);
-                    }
-                };
-            }
-            return;
-        }, []);
-
-        return (
-            <a
-                {...props}
-                ref={instance => {
-                    // first we set the local ref
-                    localRef.current = instance ?? undefined;
-
-                    // then we handle the forwarded ref
-                    // it can be a function, an object, or null
-                    if (typeof forwardedRef === 'function') {
-                        forwardedRef(instance);
-                    } else if (forwardedRef !== null) {
-                        forwardedRef.current = instance;
-                    }
-                }}
-                href={href}
-                onClick={e => {
-                    if (!href.startsWith(`#`)) {
-                        e.preventDefault();
-                        router.push(href);
-                    }
-                }}>
-                {children}
-            </a>
-        );
-    }
+  ({ href, children, ...props }, forwardedRef) => {
+    const router = useRouter();
+    const [_, startTransition] = React.useTransition();
+    return (
+      <a
+        {...props}
+        ref={forwardedRef}
+        href={href}
+        onClick={(e) => {
+          if (!href.startsWith(`#`)) {
+            e.preventDefault();
+            startTransition(() => {
+              // push should happen in a transition to not block the UI
+              router.push(href);
+            });
+          }
+        }}
+      >
+        {children}
+      </a>
+    );
+  }
 );
