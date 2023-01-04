82c03eefa28c40bffa170ba1585797bfbe07e899
Adrien KISSIE <fredkiss3@gmail.com>
2023-01-04 22:28:28 +0100

    ♻️ refactor

--- web/src/features/shared/components/next-link.tsx
+++ web/src/features/shared/components/next-link.tsx
@@ -1,53 +1,51 @@
-"use client";
-import * as React from "react";
+'use client';
+import * as React from 'react';
 // components
-import Link from "next/link";
+import Link from 'next/link';
 import {
-  LinkButton,
-  type LinkButtonProps,
-} from "@locaci/ui/components/atoms/link-button";
+    LinkButton,
+    type LinkButtonProps
+} from '@locaci/ui/components/atoms/link-button';
 
 // utils
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 // types
-import type { CustomLink } from "@locaci/ui/components/atoms/link";
+import type { CustomLink } from '@locaci/ui/components/atoms/link';
 
 export const NextLink = React.forwardRef<HTMLAnchorElement, CustomLink>(
-  (props, ref) => {
-    return <Link {...props} ref={ref} />;
-  }
+    (props, ref) => {
+        return <Link {...props} ref={ref} />;
+    }
 );
 
-export function NextLinkButton(props: Omit<LinkButtonProps, "Custom">) {
-  return <LinkButton {...props} Custom={NextLink} />;
+export function NextLinkButton(props: Omit<LinkButtonProps, 'Custom'>) {
+    return <LinkButton {...props} Custom={NextLink} />;
 }
 
-export function NextDynamicLinkButton(props: Omit<LinkButtonProps, "Custom">) {
-  return <LinkButton {...props} Custom={NextDynamicLink} />;
+export function NextDynamicLinkButton(props: Omit<LinkButtonProps, 'Custom'>) {
+    return <LinkButton {...props} Custom={NextDynamicLink} />;
 }
 
 export const NextDynamicLink = React.forwardRef<HTMLAnchorElement, CustomLink>(
-  ({ href, children, ...props }, forwardedRef) => {
-    const router = useRouter();
-    const [_, startTransition] = React.useTransition();
-    return (
-      <a
-        {...props}
-        ref={forwardedRef}
-        href={href}
-        onClick={(e) => {
-          if (!href.startsWith(`#`)) {
-            e.preventDefault();
-            startTransition(() => {
-              // push should happen in a transition to not block the UI
-              router.push(href);
-            });
-          }
-        }}
-      >
-        {children}
-      </a>
-    );
-  }
+    ({ href, children, ...props }, forwardedRef) => {
+        const router = useRouter();
+        return (
+            <a
+                {...props}
+                ref={forwardedRef}
+                href={href}
+                onClick={e => {
+                    if (!href.startsWith(`#`)) {
+                        e.preventDefault();
+                        React.startTransition(() => {
+                            // push should happen in a transition to not block the UI
+                            router.push(href);
+                        });
+                    }
+                }}>
+                {children}
+            </a>
+        );
+    }
 );
