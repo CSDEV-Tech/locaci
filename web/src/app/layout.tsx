1bfa9eac5b177acf22177592be1d5d1ce4a1bb6d
Adrien KISSIE <fredkiss3@gmail.com>
2023-01-29 19:25:44 +0100

    ⬆️ upgrade next to latest to fix an issue with autoscrolling to top

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,7 +5,6 @@ import { TrpcClientProvider } from '~/app/trpc-client-provider';
 import { TailwindIndicator } from '~/features/shared/components/tailwind-indicator';
 import { HotToaster } from '~/features/shared/components/hot-toaster';
 import { RASSRProvider } from '~/features/shared/components/react-aria-ssr-provider';
-import { ScrollUp } from './scroll-up';
 
 // utils
 import { Poppins } from '@next/font/google';
@@ -31,7 +30,6 @@ export default function RootLayout({ children }: LayoutProps) {
                 <meta name="charset" content="utf-8" />
             </head>
             <body suppressHydrationWarning>
-                <ScrollUp />
                 <RASSRProvider>
                     <TrpcClientProvider>{children}</TrpcClientProvider>
                 </RASSRProvider>
