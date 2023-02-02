592c610269a9a8663681de2b2ec39cae5dfcd8eb
Adrien KISSIE <fredkiss3@gmail.com>
2023-02-02 15:35:34 +0100

    🩹 temporary patch for next to disable hard navigation

--- web/src/features/shared/components/next-link.tsx
+++ web/src/features/shared/components/next-link.tsx
@@ -7,35 +7,12 @@ import {
     type LinkButtonProps
 } from '@locaci/ui/components/atoms/link-button';
 
-// utils
-import { Uuid } from '~/lib/uuid';
-
 // types
 import type { CustomLink } from '@locaci/ui/components/atoms/link';
 
 export const NextLink = React.forwardRef<HTMLAnchorElement, CustomLink>(
     ({ dynamic, href, ...props }, ref) => {
-        /**
-         * FIXME : this is a fix because nextjs caches pages even dynamic ones,
-         * This codes ensures to give crawlers the correct url, while making it possible for
-         * nextjs to always fetch the page, by prefixing it with a random queryString
-         */
-        const randomQueryString =
-            typeof window !== undefined ? new Uuid().short() : '';
-        const qs = new URLSearchParams();
-        if (randomQueryString) {
-            qs.append('__r', randomQueryString);
-        }
-
-        return (
-            <Link
-                {...props}
-                ref={ref}
-                // We purposelly ignore hydration errors because overwise react would shout at us
-                suppressHydrationWarning
-                href={dynamic ? `${href}?${qs.toString()}` : href}
-            />
-        );
+        return <Link {...props} ref={ref} href={href} />;
     }
 );
 
