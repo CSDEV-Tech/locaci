9e83a5824e232fedd14d030ebd953efb21d7ff91
Adrien KISSIE <fredkiss3@gmail.com>
2023-02-11 00:22:23 +0100

    🚸 some UX improvements

--- ui/src/components/atoms/card.tsx
+++ ui/src/components/atoms/card.tsx
@@ -9,29 +9,29 @@ export type CardProps = {
     onMouseLeave?: () => void;
 };
 
-export function Card({
-    className,
-    children,
-    animated = false,
-    onMouseEnter,
-    onMouseLeave
-}: CardProps) {
-    return (
-        <article
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-            className={clsx(
-                `rounded-lg bg-white shadow-md`,
-                `inline-flex`,
-                className,
-                {
-                    'transition duration-300 ease-in-out focus-within:scale-105 hover:scale-105 active:scale-105':
-                        animated,
-                    'focus-within:shadow-card hover:shadow-card active:shadow-card':
-                        animated
-                }
-            )}>
-            {children}
-        </article>
-    );
-}
+export const Card = React.forwardRef<HTMLElement, CardProps>(
+    (
+        { className, children, animated = false, onMouseEnter, onMouseLeave },
+        ref
+    ) => {
+        return (
+            <article
+                ref={ref}
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+                className={clsx(
+                    `rounded-lg bg-white shadow-md`,
+                    `inline-flex`,
+                    className,
+                    {
+                        'transition duration-300 ease-in-out focus-within:scale-105 hover:scale-105 active:scale-105':
+                            animated,
+                        'focus-within:shadow-card hover:shadow-card active:shadow-card':
+                            animated
+                    }
+                )}>
+                {children}
+            </article>
+        );
+    }
+);
