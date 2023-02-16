c57c9df7bf883b78eaf3989cc3674d3c28addaba
Adrien KISSIE <fredkiss3@gmail.com>
2023-02-16 23:13:03 +0100

    Revert "Revert "♻️ use next app routes, resolves #59""
    This reverts commit 78afc86fa87c4219e85fa7a6d85e203897626beb.

--- web/src/server/trpc/context.ts
+++ web/src/server/trpc/context.ts
@@ -1,14 +1,14 @@
 // utils
 import { getUserFromSessionToken } from '~/server/utils';
-import { getCookie } from '~/lib/functions';
 import { prisma } from '~/server/db/client';
 import { TypeSenseSearch } from '../typesense-search';
 import { env } from '~/env/server.mjs';
 
 // types
-import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
+import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import type { User } from '@prisma/client';
 import type * as trpc from '@trpc/server';
+import { cookies } from 'next/headers';
 
 export const createContext = async (
     opts:
@@ -16,7 +16,7 @@ export const createContext = async (
               type: 'rsc';
               getUser: () => Promise<User | null>;
           }
-        | (CreateNextContextOptions & { type: 'api' })
+        | (FetchCreateContextFnOptions & { type: 'api' })
 ) => {
     const searchClient = new TypeSenseSearch(
         env.TYPESENSE_SEARCH_API_KEY,
@@ -33,22 +33,17 @@ export const createContext = async (
         };
     }
 
-    const req = opts?.req;
-    const res = opts?.res;
+    const { type: _, ..._opts } = opts;
 
     // get user from cookie
     let user: User | null = null;
-    if (req?.headers.cookie) {
-        const sessionToken = getCookie('__session', req?.headers.cookie);
-
-        if (sessionToken) {
-            user = await getUserFromSessionToken(sessionToken);
-        }
+    const sessionToken = cookies().get(`__session`)?.value;
+    if (sessionToken) {
+        user = await getUserFromSessionToken(sessionToken);
     }
 
     return {
-        req,
-        res,
+        ...opts,
         prisma,
         user,
         typesense: searchClient
